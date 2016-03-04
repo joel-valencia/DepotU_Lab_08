dcb5603ab89642e115d04be7115f381b0c254332
Joel Valencia <trekkerj1@gmail.com>
2016-03-04 13:44:18 -0600

    update page on roll die

--- dice.js
+++ dice.js
@@ -3,6 +3,7 @@ var allDice = [];
 var Die = function() {
     this.roll = function() {
         this.value = Math.ceil(Math.random() * 6);
+        $('#' + this.id).text(this.value);
         return this.value;
     }
 }
@@ -12,6 +13,7 @@ $(document).ready(function() {
     $('#add').on("click", function() {
         allDice.push(new Die());
         allDice[allDice.length - 1].roll();
+        allDice[allDice.length - 1].id = allDice.length - 1;
         $('body').append('<div class="die" id=' + (allDice.length - 1) + '>' + allDice[allDice.length - 1].value + '</div>')
     });
     
